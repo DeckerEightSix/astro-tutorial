fe5f352b5751af55df04f4166a29f63902ae9aeb
DeckerEightSix <info@86deck.com>
2023-12-10 14:10:53 +0800

    7th DONE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: ""
+  site: "https://voluble-biscotti-012885.netlify.app/",
+  integrations: [preact()]
 });
